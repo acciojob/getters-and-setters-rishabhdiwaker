71a8da9c9c07857fe702c6fbc26c843af2fc02b5
rishabhdiwaker <rishabhdiwaker.rd@gmail.com>
2023-07-17 01:34:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ class Student extends Person {
 		super(name,age);
 	}
 	study() {
-	    console.log(`${this.name} is studying.`);
+	    console.log(`${this.name} is studying`);
 	}
 }
 
@@ -32,7 +32,7 @@ class Teacher extends Person {
 		super(name,age);
 	}	
 	teach() {
-		console.log(`${this.name} is teaching.`);
+		console.log(`${this.name} is teaching`);
 	}
 }
 
